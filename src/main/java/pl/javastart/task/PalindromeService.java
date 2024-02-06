d46cbdeb38edc475a3a671126fded809f570ee50
arkadiusz-wolek <arkadywolek@gmail.com>
2024-02-06 21:38:15 +0100

    rozwiązanie

--- src/main/java/pl/javastart/task/PalindromeService.java
+++ src/main/java/pl/javastart/task/PalindromeService.java
@@ -4,6 +4,14 @@ public class PalindromeService {
 
     // nie zmieniaj sygnatury tej metody. Jest ona testowana w PalindromeServiceTest
     public boolean isPalindrome(int[] array) {
-        return false;
+        if (array == null || array.length <= 1) { // tablica 1-elementowa chyba nie jest palindromem..?
+            return false;
+        }
+        for (int i = 0; i < array.length / 2; i++) {
+            if (array[i] != array[array.length - 1 - i]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
